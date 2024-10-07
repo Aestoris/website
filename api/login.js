f35f25e9e04e673debc91399ee7091e7edd2821f
aestoris <aestoris@gmail.com>
2024-10-07 19:55:33 +0200

    a

--- api/login.js
+++ api/login.js
@@ -1,13 +1,13 @@
 const querystring = require('querystring');
 
-const CLIENT_ID = process.env.DISCORD_CLIENT_ID; // Your Client ID from Discord
-const REDIRECT_URI = process.env.DISCORD_CALLBACK_URI; // Your redirect URI
+const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID; // Your Client ID from Discord
+const DISCORD_REDIRECT_URI = process.env.DISCORD_CALLBACK_URI; // Your redirect URI
 
 module.exports = (req, res) => {
     const authURL = `https://discord.com/api/oauth2/authorize?` + 
         querystring.stringify({
             client_id: DISCORD_CLIENT_ID,
-            redirect_uri: REDIRECT_URI,
+            redirect_uri: DISCORD_REDIRECT_URI,
             response_type: 'code',
             scope: 'identify'
         });
